f8ed260237ee0c4331f2da76a68d291caad5915e
GeeScot <github@gee.dev>
2023-03-08 13:38:41 +0000

    Added animated numbers library and selectFromResult to avoid too many redraws

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,37 @@
-import { skipToken } from "@reduxjs/toolkit/dist/query";
 import { useParams } from "react-router-dom";
 import styled from "styled-components";
+import { skipToken } from "@reduxjs/toolkit/dist/query";
+import AnimatedNumber from "react-animated-numbers";
 import { useGetChannelIdQuery, useGetSubscriberCountQuery } from "./features/apiSlice";
 
 const App = () => {
   const { username, goal } = useParams<{ username: string, goal: string }>();
   const { data: channelId } = useGetChannelIdQuery(username ?? skipToken);
-  const { data: subscriberCount } = useGetSubscriberCountQuery(channelId ?? skipToken, {
-    pollingInterval: 120000
+  const { subscriberCount } = useGetSubscriberCountQuery(channelId ?? skipToken, {
+    pollingInterval: 300000,
+    selectFromResult: ({ data }) => {
+      return { subscriberCount: data ?? 0 };
+    }
   });
 
   return (
     <Container>
-      <div>2023 !Youtube Sub Goal</div>
-      <Goal>{ subscriberCount } / { goal }</Goal>
+      <Header>2023 !Youtube Sub Goal</Header>
+      <Goal>
+        <AnimatedNumber
+          includeComma
+          animateToNumber={subscriberCount}
+          configs={[
+            { mass: 1, tension: 220, friction: 100 },
+            { mass: 1, tension: 180, friction: 130 },
+            { mass: 1, tension: 280, friction: 90 },
+            { mass: 1, tension: 180, friction: 135 },
+            { mass: 1, tension: 260, friction: 100 },
+            { mass: 1, tension: 210, friction: 180 },
+          ]}
+        />
+        <div>&nbsp;/&nbsp;{ goal }</div>
+      </Goal>
     </Container>
   );
 }
@@ -34,7 +52,17 @@ const Container = styled.div`
   color: #ffffff;
 `;
 
+const Header = styled.div`
+  margin-bottom: 5px;
+`;
+
 const Goal = styled.div`
+  display: flex;
+  flex-direction: row;
+
+  align-items: center;
+  justify-content: center;
+
   font-size: 20px;
   font-weight: bold;
 `;
